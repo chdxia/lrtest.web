84a32b2b2d51789aae71f7b70bbe4aacb479a9b4
chdxia <chdxiamail@gmail.com>
2022-07-06 16:58:15 +0800

    添加权限管理界面

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Layout from '@/layout'
 // import chartsRouter from './modules/charts'
 // import tableRouter from './modules/table'
 // import nestedRouter from './modules/nested'
+import permissionRouter from './modules/permission'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -152,6 +153,7 @@ export const asyncRoutes = [
   },
   /** when your routing map is too long, you can split it into small modules **/
   // tableRouter,
+  permissionRouter,
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
